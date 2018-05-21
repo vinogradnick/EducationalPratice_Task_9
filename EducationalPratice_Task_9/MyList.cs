2b4425133ac62fd4a85b7404897b7ec56a1f20ca
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-05-22 00:31:44 +0500

    new list

--- EducationalPratice_Task_9/MyList.cs
+++ EducationalPratice_Task_9/MyList.cs
@@ -12,127 +12,127 @@ namespace Task_9
      */
     class MyList
     {
-        /// <summary>
-        /// Информационное поле
-        /// </summary>
-        public int Data;
-        /// <summary>
-        /// Адрес на следующий элемент списка
-        /// </summary>
-        public MyList Next;
-
-        private static int _counter;
-        private static int _capacity;
-        public int Capacity
+        public Point First;//первый элемент списка
+        public Point Last;//последний элемент списка
+        private int count=1;//Количество элементов в списке
+        public int Size { get; set; }
+        private int _size;
+        public MyList(int size)
         {
-            get => _capacity;
-            set {if(value > 0) _capacity = value;}
+            First = null;
+            Last = null;
+            _size = size;
         }
 
-        public static int Count => _counter;//Количество элементов в списке
-        public MyList(int _data)
+        public  Point RecurrentCreate( Point Element)
         {
-            Data = _data;
-            Next = null;
-        }
-        public MyList()
-        {
-            Data = 0;
-            Next = null;
-        }
-        
-       
-        /// <summary>
-        /// Создание списка 
-        /// </summary>
-        /// <param name="array">числа для ввода</param>
-        /// <returns>Однонаправленный список</returns>
-        static MyList MakeList(int[] array)
-        {
-            int data = array[0];//1 Головной элемент списка
-            MyList start = MakePoint(data);//Задание начала списка
+            count++;
 
-            for (int i = 1; i < array.Length; i++)
+            if (_size != count)
             {
-                data = array[i];
-                MyList point = MakePoint(data);//Создание элемента списка
-                start.Next = point;//Задание ссылки на следующий элемент списка
-                start = point;
-            }
-            return start;
-        }
+              
+                if (First == null)
+                {
 
-        /// <summary>
-        /// Рекурсивное удаление элементов
-        /// </summary>
-        /// <param name="list"></param>
-        /// <param name="element"></param>
-        /// <returns></returns>
-        public static MyList RecurentRemove(MyList removeList, int element)
-        {
-            //проверка элемента на пустоту
-            if (removeList == null) return removeList;
-            //удаление первого элемента однонаправленного списка
-            if (removeList.Data == element)
-            {
-                removeList = removeList.Next;
-                return removeList;
-            }
-            //удаление элемента с 2 до предпоследнего
-            if (removeList.Next.Data == element)
-            {
-                _counter--;//уменьшить количество элементов
-                if (removeList.Next.Next == null) return removeList;//пропуск элементов
-                removeList.Next = removeList.Next.Next;//пропуск элемента для удаления
-                return removeList;
-            }
-            //переход к следующему элементу
-            return RecurentRemove(removeList.Next, element);
-        }
-        /// <summary>
-        /// Рекурентное создание списка
-        /// </summary>
-        /// <param name="list">Список для заполнения</param>
-        /// <param name="data">Информационное поле</param>
-        /// <param name="size">Размер списка</param>
-        /// <param name="counter">Счетчик списка</param>
-        /// <returns></returns>
-        public static void RecurrentCreate(int size,ref MyList list)
-        {
-            
-            if (size != _counter)
-            {
-                _counter++; //Увелечение счетчика
-                Console.Write("Введите значение для элемнента списка " + _counter + " |:=");
-
-                MyList point = MakePoint(InputValidator.InputPositive());
-                list = point;
-                RecurrentCreate(size,ref list.Next);
+                    First = Element;
+                    Console.WriteLine($"Введите {count}-е число ");
+                    int data = Input.Int32();
+                    First.Next = RecurrentCreate(new Point(data, null));
+                }
+                else
+                {
+                    Console.WriteLine($"Введите {count}-е число ");
+                    int data = Input.Int32();
+                    Element.Next = RecurrentCreate(new Point(data, null));
+                }
             }
             else
-                return;
-        }
-
-       
-        public static void Print(MyList list)
-        {
-            while (list !=null)
             {
-                Console.Write(list.Data+" ");
-                list = list.Next;
+                Console.WriteLine($"Введите {count}-е число ");
+                int data = Input.Int32();
+                Last = new Point(data,First);
+                Element.Next = Last;
+                return Element;
             }
+            return Element;
+
         }
 
-        /// <summary>
-        /// Создание элемента списка
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        public static MyList MakePoint(int data)
+        public Point Remove(int value)
         {
-            MyList list = new MyList(data);
-            return list;
+            if (First.Data == value)
+            {
+                First = First.Next;
+                Last.Next = First;
+                return First;
+            }
+            else
+            {
+                Point element = RecurrentRemove(First, value);
+                return element;
+            }
+
+        }
+        public Point RecurrentRemove(Point element, int value)
+        {
+            if (element.Next.Data == value)
+            {
+                element.Next = element.Next.Next;
+                return element;
+            }
+           return RecurrentRemove(element.Next, value);
         }
 
+        public void Print()
+        {
+            Point current = First;
+            Console.Write(current.Data+" ");
+            current = current.Next;
+            while (current != First)
+            {
+                Console.Write(current.Data+" ");
+                current = current.Next;
+            }
+
+        }
     }
+
+    public class Point
+    {
+        public int Data;
+        public Point Next;
+        public Point(int data)
+        {
+            Data = data;
+        }
+
+        public Point(int data, Point next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+
+    public static class Input
+    {
+        /// <summary>
+        /// Ввод целого числа
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int Int32()
+        {
+            int res;
+            bool ok;
+            do
+            {
+                ok = int.TryParse(Console.ReadLine(), out res);
+                ok = res > 0;
+                if (!ok)
+                    Console.WriteLine("Вы ввели неправильное число, повторите попытку \n Введите число: ");
+            } while (!ok);
+            return res;
+        }
+    }
+
 }
--- EducationalPratice_Task_9/Program.cs
+++ EducationalPratice_Task_9/Program.cs
@@ -9,16 +9,14 @@ namespace Task_9
         static void Main(string[] args)
         {
             Console.Write("Введите размер коллекции ");
-
-            MyList list = new MyList(){Capacity=InputValidator.InputPositive()};
-            
-            MyList.RecurrentCreate(list.Capacity, ref list);
-            MyList.Print(list);
-            
-            Console.Write("\nВведите элемент для удаления := ");
-            list =MyList.RecurentRemove(list,InputValidator.InputPositive());
-            MyList.Print(list);
-            Console.ReadKey();
+            int size = Input.Int32();
+            MyList list = new MyList(size);
+            int data = Input.Int32();
+            Point point = list.RecurrentCreate(new Point(data, null));
+            list.Print();
+            list.Remove(10);
+            list.Print();
+            Console.ReadLine();
         }
     }
 }
